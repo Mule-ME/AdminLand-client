47a1339c1d7f4d27182f5038a5a87317a6f32074
Mule-ME <mulualemesh15@gmail.com>
2023-09-10 21:20:34 +0300

    updating enviroment variable

---
+++ src/components/dashboard/DashboardSkeleton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Skeleton } from '@mui/material'
+
+const DashboardSkeleton = () => {
+    return (
+
+        <Skeleton height="100vh" width="100%" mt="-50px" />
+
+    )
+}
+
+export default DashboardSkeleton
