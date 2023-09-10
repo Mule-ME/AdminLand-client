a64262fcd1a7bca48170c2d88d6522e8872f45c1
Mule-ME <mulualemesh15@gmail.com>
2023-09-10 13:54:46 +0300

    feat: custom date picker implementation

--- src/components/core/index.jsx
+++ src/components/core/index.jsx
@@ -2,6 +2,6 @@ import FlexBox from "./FlexBox";
 import Header from "./Header";
 import DataGridToolbar from "./DataGridToolbar";
 import OverviewChart from "./OverviewChart";
+import CustomDatePicker from "./CustomDatePicker"
 
-
-export { FlexBox, Header, DataGridToolbar, OverviewChart }
+export { FlexBox, Header, DataGridToolbar, OverviewChart, CustomDatePicker }
